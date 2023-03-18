c54a49144ae0c12f2bd6f9d2591b9cf0a9b1a1ea
AlDm <d44.a1@yandex.ru>
2023-03-19 00:53:42 +0300

    исправить 6

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,6 @@ const popupBox = document.querySelector('.popup');
 const buttonPopupClose = document.querySelector('.popup__close');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
-const buttonPopupSubmit = document.querySelector('.popup__submit');
 const popupForm = document.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
@@ -16,12 +15,6 @@ function openPopup(){
   popupBox.classList.remove('popup_opened');
  }
 
- buttonPopupOpen.addEventListener('click', () =>{
-  openPopup();
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
- })
-
  buttonPopupClose.addEventListener('click',closePopup);
 
  popupForm.addEventListener('submit',(event)=>{
@@ -31,3 +24,8 @@ function openPopup(){
   closePopup();
  })
 
+ buttonPopupOpen.addEventListener('click', () =>{
+  openPopup();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+})
