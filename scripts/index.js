9f61d8dfea363f1f7b7c8bd848cf92739e59d898
AlDm <d44.a1@yandex.ru>
2023-03-18 19:29:41 +0300

    исправил все

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const buttonPopupOpen = document.querySelector('.profile__popup');
+const popupBox = document.querySelector('.popup');
+const buttonPopupClose = document.querySelector('.popup__close');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_job');
+const buttonPopupSubmit = document.querySelector('.popup__submit');
+const popupForm = document.querySelector('.popup__form');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+/*открыть  popup*/
+function openPopup(){
+  popupBox.classList.add('popup_opened');
+}
+/*закрыть popup*/
+ function closePopup(){
+  popupBox.classList.remove('popup_opened');
+ }
+
+ buttonPopupOpen.addEventListener('click',openPopup);
+
+ buttonPopupClose.addEventListener('click',closePopup);
+
+ popupForm.addEventListener('submit',(event)=>{
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
+ })
+
+ buttonPopupOpen.addEventListener('click', () =>{
+  openPopup();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+})
